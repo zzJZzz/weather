38c557b110eb13543cd548a650bc8ad7d6d5a4ee
zzjzzz <zzjzzz.creator@gmail.com>
2022-02-17 23:21:30 -0500

    Init commit. Trying out API and Vue

---
+++ app/controllers/api_controller.rb
@@ -0,0 +1,10 @@
+class ApiController < ApplicationController
+  # before_action :authenticate_user
+  before_action :set_default_format
+
+  private
+
+  def set_default_format
+    request.format = :json
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  namespace :api do
+    namespace :v1 do
+      resources :locations do
+        resources :recordings
+      end
+    end
+  end
+
+  resources :locations
+end
